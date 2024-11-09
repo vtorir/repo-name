cf3f0a8ae9edea5cf7f64f86f65fe94601164f13
vtorir <vika.gushh@bk.ru>
2024-11-09 13:08:02 +0300

    Fix header

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const prevBtn = document.querySelector(".prev-btn");
-  const nextBtn = document.querySelector(".next-btn");
-  const reviewsContainer = document.querySelector(".reviews-container");
-  const reviews = document.querySelectorAll(".review");
-  const reviewWidth = reviews[0].offsetWidth;
-
-  let currentIndex = 0;
-
-  // Функция для перемещения карусели влево
-  function slideLeft() {
-    if (currentIndex > 0) {
-      currentIndex--;
-      const newPosition = -currentIndex * reviewWidth;
-      reviewsContainer.style.transform = `translateX(${newPosition}px)`;
-    }
-  }
-
-  // Функция для перемещения карусели вправо
-  function slideRight() {
-    if (currentIndex < reviews.length - 1) {
-      currentIndex++;
-      const newPosition = -currentIndex * reviewWidth;
-      reviewsContainer.style.transform = `translateX(${newPosition}px)`;
-    }
-  }
-
-  // Обработчики событий для кнопок "Предыдущий" и "Следующий"
-  prevBtn.addEventListener("click", slideLeft);
-  nextBtn.addEventListener("click", slideRight);
-});
-
-
-
-
 // Получение ссылок на элементы
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("openModalBtn");
@@ -54,4 +19,5 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
 
